8133f4b9b681af3fb2b2aa42d1f4bb67acccaf19
Justin Svegliato <justin.svegliato1@marist.edu>
2013-09-25 23:29:29 -0400

    Modified settings.py

--- opensr/settings.py
+++ opensr/settings.py
@@ -89,7 +89,7 @@ MIDDLEWARE_CLASSES = (
     #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-CKEDITOR_UPLOAD_PATH = 'opensr/media/images'
+CKEDITOR_UPLOAD_PATH = '/media/images'
 
 CKEDITOR_CONFIGS = {
     'default': {
@@ -159,11 +159,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# import os
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+# 
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
