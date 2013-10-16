fa965a986c1ceca37a704f040489198f4ab6e6b1
Justin Svegliato <justin.svegliato1@marist.edu>
2013-10-15 23:23:05 -0400

    [Closes #47] Refactored testing logic

--- test/static/js/test.js
+++ test/static/js/test.js
@@ -1,54 +1,73 @@
 $(document).ready(function() {
-    Phase = {
-        INSTRUCTION: 0,
-        TESTING: 1,
-        TERMINATION: 2
-    };
-
-    initializeInstructionPhase();
+    Test.initializeInstructionPhase();
     
     $(document).keypress(function(event) {
-        if (phase !== Phase.TERMINATION) {
+        if (Test.phase !== Test.Phase.TERMINATION) {
             var keyPressed = event.keyCode ? event.keyCode : event.which;
-            var START_KEY_BIND = 32;
-            var LEFT_KEY_BINDS = [
-                test[0].fields.left_key_bind.toLowerCase().charCodeAt(0),
-                test[0].fields.left_key_bind.toUpperCase().charCodeAt(0)
-            ];
-            var RIGHT_KEY_BINDS = [
-                test[0].fields.right_key_bind.toLowerCase().charCodeAt(0),
-                test[0].fields.right_key_bind.toUpperCase().charCodeAt(0)
-            ];
-            if (keyPressed === START_KEY_BIND && phase === Phase.INSTRUCTION) {
-                initializeTestingPhase();
-            } else if ($.inArray(keyPressed, LEFT_KEY_BINDS.concat(RIGHT_KEY_BINDS)) >= 0 && phase === Phase.TESTING) {
-                var correctRightCategory = $.inArray(keyPressed, RIGHT_KEY_BINDS) >= 0 && $.inArray(stimulus.fields.category, getIds(rightCategories)) >= 0;
-                var correctLeftCategory = $.inArray(keyPressed, LEFT_KEY_BINDS) >= 0 && $.inArray(stimulus.fields.category, getIds(leftCategories)) >= 0;
+            if (keyPressed === Test.START_KEY_BIND && Test.phase === Test.Phase.INSTRUCTION) {
+                Test.initializeTestingPhase();
+            } else if ($.inArray(keyPressed, Test.LEFT_KEY_BINDS.concat(Test.RIGHT_KEY_BINDS)) >= 0 && Test.phase === Test.Phase.TESTING) {
+                var correctRightCategory = $.inArray(keyPressed, Test.RIGHT_KEY_BINDS) >= 0 && $.inArray(Test.stimulus.fields.category, Test.getIds(Test.rightCategories)) >= 0;
+                var correctLeftCategory = $.inArray(keyPressed, Test.LEFT_KEY_BINDS) >= 0 && $.inArray(Test.stimulus.fields.category, Test.getIds(Test.leftCategories)) >= 0;
                 if (correctRightCategory || correctLeftCategory) {
-                    handleCorrectAnswer();
-                    if (stimulusCount > block.fields.number_of_stimuli) {
-                        if (blocks.length > 0) {
-                            initializeInstructionPhase();
+                    Test.handleCorrectAnswer();
+                    if (Test.stimulusCount > Test.block.fields.number_of_stimuli) {
+                        if (Test.blocks.length > 0) {
+                            Test.initializeInstructionPhase();
                         } else {
-                            initializeTerminationPhase();
+                            Test.initializeTerminationPhase();
                         }
                     }
                 } else {
-                    handleIncorrectAnswer();
+                    Test.handleIncorrectAnswer();
                 }
             }
         }
     });
 });
 
-function handleBlock() {
+function Test() {};
+
+Test.Phase = {
+    INSTRUCTION: 0,
+    TESTING: 1,
+    TERMINATION: 2
+};
+
+Test.phase = Test.Phase.INSTRUCTION;
+Test.test = test;
+Test.blocks = blocks;
+Test.categories = categories;
+Test.stimuli = stimuli;
+Test.media_url = media_url;
+
+Test.leftCategories = null;
+Test.rightCategories = null;
+Test.block = null;
+Test.stimulus = null;
+Test.previousStimulus = null;
+Test.stimulusCount = null;
+Test.startTime = null;
+Test.correct = null;
+
+Test.START_KEY_BIND = 32;
+Test.LEFT_KEY_BINDS = [
+    Test.test[0].fields.left_key_bind.toLowerCase().charCodeAt(0),
+    Test.test[0].fields.left_key_bind.toUpperCase().charCodeAt(0)
+];
+Test.RIGHT_KEY_BINDS = [
+    Test.test[0].fields.right_key_bind.toLowerCase().charCodeAt(0),
+    Test.test[0].fields.right_key_bind.toUpperCase().charCodeAt(0)
+];
+
+Test.handleBlock = function() {
     for (var order = 1; order < 10; order++) {
-        var blockGroup = $.grep(blocks, function(n) {
+        var blockGroup = $.grep(Test.blocks, function(n) {
             return n.fields.order === order;
         });
         if (blockGroup !== null && blockGroup.length !== 0) {
             var block = blockGroup[Math.floor(Math.random() * blockGroup.length)];
-            blocks = blocks.filter(function(item) {
+            Test.blocks = Test.blocks.filter(function(item) {
                 return item.pk !== block.pk;
             });
             return block;
@@ -56,21 +75,21 @@ function handleBlock() {
     }
 }
 
-function handleStimulus(leftCategories, rightCategories) {
-    var categoryIds = getIds(leftCategories).concat(getIds(rightCategories));
-    var filteredStimuli = $.grep(stimuli, function(n) {
+Test.handleStimulus = function(leftCategories, rightCategories) {
+    var categoryIds = Test.getIds(leftCategories).concat(Test.getIds(rightCategories));
+    var filteredStimuli = $.grep(Test.stimuli, function(n) {
         return $.inArray(n.fields.category, categoryIds) >= 0;
     });
     var stimulus = filteredStimuli[Math.floor(Math.random() * filteredStimuli.length)];
     var html = (stimulus.model === "portal.imagestimulus") 
-          ? '<img class="image-stimulus" src="' + media_url + stimulus.fields.value + '" alt="picture" />'
+          ? '<img class="image-stimulus" src="' + Test.media_url + stimulus.fields.value + '" alt="picture" />'
           : '<span class="text-stimulus">' + stimulus.fields.value + "</span>";
     $("#stimulus").html(html);
     return stimulus;
 }
 
-function handleCategory(block, categoryType) {
-    var filteredCategories = $.grep(categories, function(n) {
+Test.handleCategory = function(block, categoryType) {
+    var filteredCategories = $.grep(Test.categories, function(n) {
         return n.pk === block.fields["primary_" + categoryType + "_category"] || n.pk === block.fields["secondary_" + categoryType + "_category"];
     });
     $("#primary-" + categoryType + "-category").html(filteredCategories[0].fields.category_name).css("color", filteredCategories[0].fields.color);
@@ -83,40 +102,40 @@ function handleCategory(block, categoryType) {
     return filteredCategories;
 }
 
-function initializeTerminationPhase() {
-    phase = Phase.TERMINATION;
+Test.initializeTerminationPhase = function() {
+    Test.phase = Test.Phase.TERMINATION;
     $("#testing-phase-container").hide();
     $("#termination-phase-container").show();
-    data = {'test_status': true};
+    var data = {'test_status': true};
     $.get("../record/test-status/", data);
 }
 
-function initializeInstructionPhase() {
-    phase = Phase.INSTRUCTION;
-    block = handleBlock();
-    stimulusCount = 0;
-    $("#instructions").html(block.fields.instructions);
+Test.initializeInstructionPhase = function() {
+    Test.phase = Test.Phase.INSTRUCTION;
+    Test.block = Test.handleBlock();
+    Test.stimulusCount = 0;
+    $("#instructions").html(Test.block.fields.instructions);
     $("#testing-phase-container").hide();
     $("#instruction-phase-container").show();
     $("#status").css("visibility", "hidden");
 }
 
-function initializeTestingPhase() {
-    phase = Phase.TESTING;
-    leftCategories = handleCategory(block, "left");
-    rightCategories = handleCategory(block, "right");
-    stimulus = handleStimulus(leftCategories, rightCategories);
-    previousStimulus = stimulus;
-    stimulusCount = 1;
-    startTime = new Date().getTime();
-    correct = true;
+Test.initializeTestingPhase = function() {
+    Test.phase = Test.Phase.TESTING;
+    Test.leftCategories = Test.handleCategory(Test.block, "left");
+    Test.rightCategories = Test.handleCategory(Test.block, "right");
+    Test.stimulus = Test.handleStimulus(Test.leftCategories, Test.rightCategories);
+    Test.previousStimulus = Test.stimulus;
+    Test.stimulusCount = 1;
+    Test.startTime = new Date().getTime();
+    Test.correct = true;
     $("#instruction-phase-container").hide();
     $("#testing-phase-container").show();
 }
 
-function handleCorrectAnswer() {
+Test.handleCorrectAnswer = function() {
     function record_trial(block, leftCategories, rightCategories, stimulus, latency, correct) {
-        data = {
+        var data = {
             "block": block.fields.block_name,
             "practice": block.fields.practice,
             "primary_left_category": leftCategories[0].fields.category_name,
@@ -130,22 +149,22 @@ function handleCorrectAnswer() {
         $.get("../record/trial/", data);
     }
 
-    record_trial(block, leftCategories, rightCategories, stimulus, new Date().getTime() - startTime, correct);
-    startTime = new Date().getTime();
-    while (stimulus === previousStimulus) {
-         stimulus = handleStimulus(leftCategories, rightCategories);
+    record_trial(Test.block, Test.leftCategories, Test.rightCategories, Test.stimulus, new Date().getTime() - Test.startTime, Test.correct);
+    Test.startTime = new Date().getTime();
+    while (Test.stimulus === Test.previousStimulus) {
+         Test.stimulus = Test.handleStimulus(Test.leftCategories, Test.rightCategories);
     }
-    previousStimulus = stimulus;
-    stimulusCount++;
+    Test.previousStimulus = Test.stimulus;
+    Test.stimulusCount++;
     $("#status").css("visibility", "hidden");
 }
 
-function handleIncorrectAnswer() {
-    correct = false;
+Test.handleIncorrectAnswer = function() {
+    Test.correct = false;
     $("#status").css("visibility", "visible");
 }
 
-function getIds(list) {
+Test.getIds = function(list) {
     var ids = [];
     $.each(list, function(index, value) {
         ids.push(value.pk);
