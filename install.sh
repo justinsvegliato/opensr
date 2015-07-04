eeee4caf109ed068a25613cc590ef83d00e6a13e
Justin Svegliato <justin.svegliato1@marist.edu>
2015-07-04 20:30:30 +0000

    Created basic installation script

---
+++ install.sh
@@ -0,0 +1,6 @@
+echo "Installing OpenSR on Cloud9 IDE..."
+sudo pip install Django==1.5.0
+sudo apt-get build-dep python-psycopg2
+sudo pip install psycopg2
+sudo pip install django-sortedm2m
+echo "Finished installation!"
